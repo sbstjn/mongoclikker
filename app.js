43ec0951a3aa8365290e85b7f3282d3affeba331
semu <s@semu.mp>
2011-07-28 21:37:15 +0200

    Changed app.js

--- app.js
+++ app.js
@@ -2,7 +2,5 @@ var mongoclikker = require('./mongoclikker.js');
 
 mongoclikker/*.setHost('localhost')
             .andPort(27017)
-            .withUser('')
-            .andPassword('')
             .forDatabase('mongoclikker')
             */.andListenOn(2002);
