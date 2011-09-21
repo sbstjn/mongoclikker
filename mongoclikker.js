50801bc93bbf62c703038e306a57d5af8d9a25de
semu <s@semu.mp>
2011-09-21 22:08:07 +0000

    clean-up

--- mongoclikker.js
+++ mongoclikker.js
@@ -1,3 +1,6 @@
+/**
+ * Default mongoclikker connection settings
+ * */
 var mongoclickkerConnection = {
   host    : 'localhost'
 , port    : '27017'
@@ -58,7 +61,9 @@ var funcStartMongoclikker = function() {
     , viewURL = '/view/';
   
   if (BSON == null) {
-    // no native support, fall back to pure mode
+    /**
+     * Fallback for non-native BSON objects
+     * */
     BSON = require('mongodb').BSONPure;
     connectionSettings = {};
   }
@@ -67,10 +72,16 @@ var funcStartMongoclikker = function() {
     , currentHostname = mongoclickkerConnection.host
     , currentPort     = mongoclickkerConnection.port;
   
+  /**
+   * Serve style.css file
+   * */
   app.get('/style.css', function(req, res) {
     res.end("@import url('http://fonts.googleapis.com/css?family=Varela+Round&v2'); .canEdit { cursor: pointer; } .canEdit input { font-family: 'Varela Round', sans-serif; font-size: 14px; font-weight: normal; padding: 0 0 0 2px; margin: 0px; border: 0px; background-color: #CCC; } body { background-color: #EEEEEE; color: #36393D; font-family: 'Varela Round', sans-serif; font-size: 12px; font-weight: normal; margin: 30px; } a { text-decoration: none; color: #bf1010; } a:hover { text-decoration: underline; } .desc { font-weight: bold; vertical-align: top; font-size: 14px; font-weight: normal; } .content { font-size: 14px; font-weight: normal; padding-left: 25px; } .content ul { list-style-type: none; margin: 0; padding: 0; } .content ul li { margin: 0; padding: 0; } .docsNav { font-size: 14px; font-weight: normal; } .docsNav td { padding-top: 15px; padding-bottom: 25px; } .key { color: #999; } tr:hover .key { color: #36393D; }");
   });
   
+  /**
+   * Load script.js from external file
+   * */
   app.get('/script.js', function(req, res) {
     var fs = require('fs');
     fs.readFile(__dirname + '/script.js', function(error, content) {
@@ -83,14 +94,22 @@ var funcStartMongoclikker = function() {
     });
   });
   
+  /**
+   * Redirect / to viewURL
+   * */
   app.get('/', function(req, res) {
     res.redirect(viewURL);
   });
   
+  /**
+   * Handle database access
+   * */
   app.get(viewURL + ':curDB?/:curCollection?/:curStart?/:curLimit?/:curDocument?/:curKey?/:subID?', function(req, res, next) {
     res.write('<html><head><title>mongoclikker</title><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/><script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script><script src="/script.js"></script><link rel="stylesheet" href="/style.css"></head>');
   
-    // don't know how to get `show dbs` with node yet, so start with one datebase
+    /**
+     * don't know how to get `show dbs` with node yet, so start with one datebase
+     * */
     var dbName = req.params.curDB || currentDatabase
       , listDB = [dbName]
       , db = new Db(dbName, new Server(currentHostname, currentPort, {}), connectionSettings)
@@ -101,13 +120,17 @@ var funcStartMongoclikker = function() {
     res.write('</ul></td></tr>');
 
     if (!req.params.curDB) {
+      /**
+       * No database selected
+       * */
       endResponse(res);
     } else {
-      // db selected
+      /**
+       * Selected database
+       * */
       path += req.params.curDB + '/';
       db.open(function(err, ignored) {
-        if (err) {
-          console.log(err); }
+        if (err) { console.log(err); }
         db.collectionNames(function(err, names) {
           var colItems = [];
           res.write('<tr><td class="desc">collection</td><td class="content"><ul id="collection">');
@@ -118,84 +141,96 @@ var funcStartMongoclikker = function() {
           }
           
           if (!req.params.curCollection) {
+            /**
+             * No collection selected
+             * */
             endResponse(res);
           } else {
-            // collection selected
+            /**
+             * Selected collection
+             * */
             path += req.params.curCollection + '/' + req.params.curStart + '/' + req.params.curLimit + '/';
-            var db = new Db(dbName, new Server(currentHostname, currentPort, {}), connectionSettings);
-            db.open(function(err, db) {
-              db.collection(req.params.curCollection, function(err, collection) {
-                collection.find({}, {'skip':req.params.curStart, 'limit':req.params.curLimit}).toArray(function(err, results) {
-                  res.write('<tr><td class="desc">item</td><td class="content"><ul id="documents">');
-                  for (var i = 0; i < results.length; i++) { 
-                    if (results[i]) { res.write('<li><a href="' + path + results[i]._id + '">' + (results[i].name && typeof(results[i].name) == 'string' ? results[i].name + ' (#' + results[i]._id + ')' : results[i]._id) + '</a></li>'); } }
-                  res.write('</ul></td></tr>');
-                  var prevStart = (req.params.curStart*1 - 1*req.params.curLimit);
-                  if (prevStart < 0) { 
-                    prevStart = 0;  }
-                  if (req.params.curDocument) { 
-                    selectedItem = '/' + req.params.curDocument; }
-                  
-                  var selectedItem = ''
-                    , baseURL = viewURL + req.params.curDB + '/' + req.params.curCollection + '/'
-                    , nextURL = baseURL + (req.params.curStart*1 + 1*req.params.curLimit) + '/' + req.params.curLimit + selectedItem
-                    , prevURL = baseURL + prevStart + '/' + req.params.curLimit + selectedItem;
-                  res.write('<tr class="docsNav"><td class="desc"></td><td class="content">');
-                  
-                  if (req.params.curStart > 0) { 
-                    res.write('<a href="' + prevURL + '">prev</a> | '); }
-                  if (results.length >= req.params.curLimit) {
-                    res.write('<a href="' + nextURL + '">next</a></td></tr>'); }  
-                  
-                  if (!req.params.curDocument) {
-                    res.write('</table>');
-                    endResponse(res); 
-                  } else {
-                    // document selected
-                    var db = new Db(dbName, new Server(currentHostname, currentPort, {}), connectionSettings);
-                    db.open(function(err, db) {
-                      db.collection(req.params.curCollection, function(err, collection2) {
-                        params = {'_id': req.params.curDocument};
-                        if (req.params.curDocument.length == 12 || req.params.curDocument.length == 24) { 
-                          params = {'_id': new  BSON.ObjectID(req.params.curDocument)}; }
-                        collection.find(params).toArray(function(err, results) {
-                          for (var n in results[0]) { 
-                            if (results[0][n] instanceof Array) {
-                              res.write('<tr><td class="desc key">' + n + '</td><td class="content value"><ul>');
-                              for (var s in results[0][n]) {
-                                var current0NSItem = results[0][n][s];
-                                if (current0NSItem._id) {
-                                  if (current0NSItem.name) {
-                                    res.write('<li><a href="' + path + req.params.curDocument + '/' + n + '/' + current0NSItem._id + '">');
-                                    res.write(current0NSItem.name + ' (#' + current0NSItem._id + ')</a>'); }
-                                  else if (current0NSItem._id) {
-                                    res.write('<li><a href="' + path + req.params.curDocument + '/' + n + '/' + current0NSItem._id + '">');
-                                    res.write(current0NSItem._id + '</a>'); 
-                                  }
-                                 
-                                  if (req.params.subID && current0NSItem._id == req.params.subID) {
-                                    res.write('<table>');
-                                    for (var key in current0NSItem) {
-                                      res.write('<tr><td class="desc key">' + key + '</td><td class="content value subValue canEdit"  id="' + req.params.curDocument + '_' + n + '_' + current0NSItem._id + '_' + key + '">');
-                                      res.write(current0NSItem[key] + '</td></tr>');
-                                    }
-                                    res.write('</table>');
-                                  }
-                                  res.write('</li>');
-                                }
+            db.collection(req.params.curCollection, function(err, collection) {
+              collection.find({}, {'skip':req.params.curStart, 'limit':req.params.curLimit}).toArray(function(err, results) {
+                res.write('<tr><td class="desc">item</td><td class="content"><ul id="documents">');
+                for (var i = 0; i < results.length; i++) { 
+                  if (results[i]) { res.write('<li><a href="' + path + results[i]._id + '">' + (results[i].name && typeof(results[i].name) == 'string' ? results[i].name + ' (#' + results[i]._id + ')' : results[i]._id) + '</a></li>'); } }
+                res.write('</ul></td></tr>');
+                var prevStart = (req.params.curStart*1 - 1*req.params.curLimit);
+                if (prevStart < 0) { 
+                  prevStart = 0;  }
+                if (req.params.curDocument) { 
+                  selectedItem = '/' + req.params.curDocument; }
+                
+                /**
+                 * Build navigation items
+                 * */
+                var selectedItem = ''
+                  , baseURL = viewURL + req.params.curDB + '/' + req.params.curCollection + '/'
+                  , nextURL = baseURL + (req.params.curStart*1 + 1*req.params.curLimit) + '/' + req.params.curLimit + selectedItem
+                  , prevURL = baseURL + prevStart + '/' + req.params.curLimit + selectedItem;
+                res.write('<tr class="docsNav"><td class="desc"></td><td class="content">');
+                if (req.params.curStart > 0) { 
+                  res.write('<a href="' + prevURL + '">prev</a> | '); }
+                if (results.length >= req.params.curLimit) {
+                  res.write('<a href="' + nextURL + '">next</a></td></tr>'); }  
+                
+                if (!req.params.curDocument) {
+                  /**
+                   * No selected document
+                   * */
+                  res.write('</table>');
+                  endResponse(res); 
+                } else {
+                  /**
+                   * Selected document
+                   * */
+                  db.collection(req.params.curCollection, function(err, collection2) {
+                    params = {'_id': req.params.curDocument};
+                    if (req.params.curDocument.length == 12 || req.params.curDocument.length == 24) { 
+                      params = {'_id': new  BSON.ObjectID(req.params.curDocument)}; }
+                    collection.find(params).toArray(function(err, results) {
+                      for (var n in results[0]) { 
+                        if (results[0][n] instanceof Array) {
+                          /**
+                           * Document has sub documents
+                           * */
+                          res.write('<tr><td class="desc key">' + n + '</td><td class="content value"><ul>');
+                          for (var s in results[0][n]) {
+                            var current0NSItem = results[0][n][s];
+                            if (current0NSItem._id) {
+                              if (current0NSItem.name) {
+                                res.write('<li><a href="' + path + req.params.curDocument + '/' + n + '/' + current0NSItem._id + '">');
+                                res.write(current0NSItem.name + ' (#' + current0NSItem._id + ')</a>'); }
+                              else if (current0NSItem._id) {
+                                res.write('<li><a href="' + path + req.params.curDocument + '/' + n + '/' + current0NSItem._id + '">');
+                                res.write(current0NSItem._id + '</a>'); 
                               }
-                              res.write('</ul></td></tr>');
-                            } else {
-                              res.write('<tr><td class="desc key">' + n + '</td><td class="content value propValue canEdit" id="' + req.params.curDocument + '_' + n + '">' + results[0][n] + '</td></tr>'); 
+                             
+                              if (req.params.subID && current0NSItem._id == req.params.subID) {
+                                res.write('<table>');
+                                for (var key in current0NSItem) {
+                                  res.write('<tr><td class="desc key">' + key + '</td><td class="content value subValue canEdit"  id="' + req.params.curDocument + '_' + n + '_' + current0NSItem._id + '_' + key + '">');
+                                  res.write(current0NSItem[key] + '</td></tr>');
+                                }
+                                res.write('</table>');
+                              }
+                              res.write('</li>');
                             }
                           }
-                          res.write('</table>');
-                          endResponse(res); 
-                        });
-                      });
+                          res.write('</ul></td></tr>');
+                        } else {
+                          /**
+                           * Display simple document property
+                           * */
+                          res.write('<tr><td class="desc key">' + n + '</td><td class="content value propValue canEdit" id="' + req.params.curDocument + '_' + n + '">' + results[0][n] + '</td></tr>'); 
+                        }
+                      }
+                      res.write('</table>');
+                      endResponse(res); 
                     });
-                  }
-                });
+                  });
+                }
               });
             });
           }
